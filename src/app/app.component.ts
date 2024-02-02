edae6042960ec787a12f33f21b230d88dda97ae7
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-02-02 18:51:09 -0300

    feat(feature): add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { LayoutModule } from '@webartestudio/layout';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, LayoutModule],
   selector: 'webartestudio-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
