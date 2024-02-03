dad980f46e9503bf8db104b8ad9c24bfe8bf2a2a
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-02-02 21:13:10 -0300

    refactor(app): refactor to home component

--- modules/feature/home/src/lib/home/home.component.ts
+++ modules/feature/home/src/lib/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { LayoutModule } from '../../../../layout/src/lib/layout.module';
 
 @Component({
   selector: 'webartestudio-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LayoutModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
   {
     path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: '',
     loadChildren: () => import('@webartestudio/home').then((m) => m.homeRoutes),
   },
 ];
