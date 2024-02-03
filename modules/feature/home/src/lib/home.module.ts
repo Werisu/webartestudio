363e29b4e51c60bd8ba09147dd280db749d1e7b3
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-02-02 21:04:05 -0300

    feat(home): add component

--- modules/feature/home/src/lib/home.module.ts
+++ modules/feature/home/src/lib/home.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { homeRoutes } from './lib.routes';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(homeRoutes)],
+  imports: [CommonModule, RouterModule.forChild(homeRoutes), HomeComponent],
+  exports: [HomeComponent],
 })
 export class HomeModule {}
