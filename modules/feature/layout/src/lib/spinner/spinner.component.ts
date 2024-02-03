a4e4f1a5387dbea52f67a368992ac45d019a0bb9
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-02-02 23:39:21 -0300

    feat(layout): spinner component

---
+++ modules/feature/layout/src/lib/spinner/spinner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'webartestudio-spinner',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './spinner.component.html',
+  styleUrl: './spinner.component.css',
+})
+export class SpinnerComponent {}
